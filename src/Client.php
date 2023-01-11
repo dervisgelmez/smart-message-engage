f49ab2db04c300643f816ed4eaca7a1c2b8800bb
dervisgelmez <dervisgelmez@yandex.com>
2023-01-12 00:03:45 +0300

    Update - curl request fixed

--- src/Client.php
+++ src/Client.php
@@ -26,16 +26,23 @@ class Client
     {
         $body = $request->getRequestBody();
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, self::API);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: text/xml; charset=utf-8", "Content-Length: " . strlen($body)));
-        $output = curl_exec($ch);
-        curl_close($ch);
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => self::API,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 1,
+            CURLOPT_TIMEOUT => 20,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $body,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: text/xml'
+            )
+        ));
+        $output = curl_exec($curl);
+        curl_close($curl);
 
         return $request->getResponse($output);
     }
